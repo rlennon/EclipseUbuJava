6b70fa0500e6c26becb59721489ef4fb9d3454f5
Ruth Lennon <ruth.lennon@lyit.ie>
2019-11-10 22:43:36 +0000

    Initial Project Commit

---
+++ JavaPrjGitEclipse/src/pkg/Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * @author l0012345
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings LYIT Student");
+	}
+
+}
